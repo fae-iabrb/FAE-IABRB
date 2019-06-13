966dcb6c06b119e2d5c4f21931c4211dbb3b268f
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-06-13 00:38:11 +0000

    layout para pagina padrão de conteudo

---
+++ page.php
@@ -0,0 +1,34 @@
+<?php /* Template Name: Padrão */ ?>
+
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="pageHeader">
+  <h1><?php the_title();?></h1>
+</div>
+
+
+  <div class="pageMain container">
+
+    <div class="pageContent">
+
+
+
+    <?php the_content();
+
+
+
+endwhile; else: ?>
+<p>Sorry, no posts matched your criteria.</p>
+
+
+
+
+<?php endif; ?>
+
+</div>
+  </div>
+
+
+<?php get_footer(); ?>
