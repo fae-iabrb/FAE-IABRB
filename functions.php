ea997f2602d0c2c05c693e9786ed4aa77f71fb00
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-06-23 22:19:33 -0300

    update

--- functions.php
+++ functions.php
@@ -24,11 +24,7 @@ add_action( 'init', 'wpb_custom_new_menu' );
 
 add_theme_support( 'post-formats', array( 'image', 'link' ) );
 
-/**
- * Register a custom post type called "Banner".
- *
- * @see get_post_type_labels() for label keys.
- */
+
 function banner_init() {
   $labels = array(
       'name'                  => _x( 'Banners', 'Post type general name', 'textdomain' ),
@@ -80,9 +76,9 @@ add_action( 'init', 'banner_init' );
 
 function event_init() {
   $labels = array(
-      'name'                  => _x( 'Events', 'Post type general name', 'textdomain' ),
-      'singular_name'         => _x( 'Event', 'Post type singular name', 'textdomain' ),
-      'menu_name'             => _x( 'Events', 'Admin Menu text', 'textdomain' ),
+      'name'                  => _x( 'Inscrições de Eventos', 'Post type general name', 'textdomain' ),
+      'singular_name'         => _x( 'Inscrição de Evento', 'Post type singular name', 'textdomain' ),
+      'menu_name'             => _x( 'Inscrições de Eventos', 'Admin Menu text', 'textdomain' ),
       'name_admin_bar'        => _x( 'Event', 'Add New on Toolbar', 'textdomain' ),
       'add_new'               => __( 'Add New', 'textdomain' ),
       'add_new_item'          => __( 'Add New Event', 'textdomain' ),
@@ -131,8 +127,10 @@ add_action( 'init', 'event_init' );
 function register_quickmenu_post_type() {
   // Set various pieces of text, $labels is used inside the $args array
   $labels = array(
-     'name' => _x( 'QuickMenus', 'post type general name' ),
-     'singular_name' => _x( 'Movie', 'post type singular name' )
+     'name' => _x( 'Menus rápido', 'post type general name' ),
+     'singular_name' => _x( 'Menu rápido', 'post type singular name' ),
+     'menu_name'  => _x( 'Menus rápidos', 'Admin Menu text', 'textdomain' ),
+
      
   );
   // Set various pieces of information about the post type
@@ -149,6 +147,56 @@ function register_quickmenu_post_type() {
 
 // The custom function MUST be hooked to the init action hook
 add_action( 'init', 'register_quickmenu_post_type' );
+
+
+function schedule_init() {
+  $labels = array(
+      'name'                  => _x( 'Agenda', 'Post type general name', 'textdomain' ),
+      'singular_name'         => _x( 'Agenda', 'Post type singular name', 'textdomain' ),
+      'menu_name'             => _x( 'Agenda', 'Admin Menu text', 'textdomain' ),
+      'name_admin_bar'        => _x( 'Schedule', 'Add New on Toolbar', 'textdomain' ),
+      'add_new'               => __( 'Add New', 'textdomain' ),
+      'add_new_item'          => __( 'Add New Schedule', 'textdomain' ),
+      'new_item'              => __( 'New Schedule', 'textdomain' ),
+      'edit_item'             => __( 'Edit Schedule', 'textdomain' ),
+      'view_item'             => __( 'View Schedule', 'textdomain' ),
+      'all_items'             => __( 'All schedule', 'textdomain' ),
+      'search_items'          => __( 'Search schedule', 'textdomain' ),
+      'parent_item_colon'     => __( 'Parent schedule:', 'textdomain' ),
+      'not_found'             => __( 'No schedule found.', 'textdomain' ),
+      'not_found_in_trash'    => __( 'No schedule found in Trash.', 'textdomain' ),
+      'featured_image'        => _x( 'Schedule Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+      'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+      'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+      'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+      'archives'              => _x( 'Schedule archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+      'insert_into_item'      => _x( 'Insert into Schedule', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+      'uploaded_to_this_item' => _x( 'Uploaded to this Schedule', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+      'filter_items_list'     => _x( 'Filter schedule list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+      'items_list_navigation' => _x( 'schedule list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+      'items_list'            => _x( 'schedule list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+  );
+
+  $args = array(
+      'labels'             => $labels,
+      'public'             => true,
+      'publicly_queryable' => true,
+      'show_ui'            => true,
+      'show_in_menu'       => true,
+      'query_var'          => true,
+      'rewrite'            => array( 'slug' => 'schedule' ),
+      'capability_type'    => 'post',
+      'has_archive'        => true,
+      'hierarchical'       => false,
+      'menu_position'      => null,
+      'menu_icon'           => 'dashicons-format-gallery',
+      'supports'           => array( 'title', 'editor', 'page-attributes'),
+  );
+
+  register_post_type( 'schedule', $args );
+}
+
+add_action( 'init', 'schedule_init' );
 
 ?>
 
