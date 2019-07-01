6aa21e31771a2a50c789074747767d96fb067b8c
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-06-30 23:10:55 -0300

    add portais no footer

--- footer.php
+++ footer.php
@@ -100,6 +100,36 @@ if ($refractedMenus) {
 
 ?>      
 
+<div class="col">
+        <h1>Portais</h1>
+        <ul>
+
+        <?php 
+
+$menu_name = 'portals';
+
+$menu_list = '';
+
+if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+    $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+
+    $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+    foreach ( (array) $menu_items as $key => $menu_item ) {
+        $title = $menu_item->title;
+        $url = $menu_item->url;
+        $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>'; 
+    }
+
+    echo $menu_list;
+} 
+
+?>
+         
+        </ul>
+      </div>
+
+
       <div class="clearfix"></div>
     </div>
   </div>
